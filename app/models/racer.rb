d68c0fb8aa932ed567333fed9849799dcc301a25
wouwou4444 <wouwou4444@outlook.com>
2017-07-25 01:18:09 +0200

    controller_view

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,11 @@
 class Racer
+    include ActiveModel::Model
 
     attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+    def persisted?
+        !@id.nil?
+    end
 
     def self.mongo_client
         @@db = Mongoid::Clients.default 
@@ -12,11 +17,11 @@ class Racer
 
     def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
         if ( limit.nil? == true )
-            @@col.find(prototype)
+            self.collection.find(prototype)
             .sort(sort)
             .skip(skip)
         else
-             @@col.find(prototype)
+             self.collection.find(prototype)
             .sort(sort)
             .skip(skip)
             .limit(limit)
@@ -41,18 +46,42 @@ class Racer
     end
 
     def self.find (id)
-        result = (id.is_a? String) ? @@col.find(:_id=>BSON::ObjectId.from_string(id)).first : @@col.find(_id: id).first
+        result = (id.is_a? String) ? self.collection.find(:_id=>BSON::ObjectId.from_string(id)).first : self.collection.find(_id: id).first
         return result.nil? ? nil : Racer.new(result)
     end
 
     def save
-        result = @@col.insert_one(number:@number, first_name:@first_name, last_name:@last_name, gender: @gender, group:@group, secs:@secs)
-        @id = @@col.find(number:@number, first_name:@first_name, last_name:@last_name, gender: @gender, group:@group, secs:@secs).first[:_id].to_s
+        result = Racer.collection.insert_one(number:@number, first_name:@first_name, last_name:@last_name, gender: @gender, group:@group, secs:@secs)
+        @id = Racer.collection.find(number:@number, first_name:@first_name, last_name:@last_name, gender: @gender, group:@group, secs:@secs).first[:_id].to_s
     end
 
     #
     def update (params)
+        @number=params[:number].to_i
+
+        @first_name=params[:first_name]
+
+        @last_name=params[:last_name]
+
+        @secs=params[:secs].to_i
+
+        @gender=params[:gender]
+        @group=params[:group]
+
+        params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+        Racer.collection.find(_id:BSON::ObjectId.from_string(@id)).update_one(params)
 
     end
 
+    def destroy
+         Racer.collection.find(:number=>(@number)).delete_one
+    end
+
+    def created_at
+        nil
+    end
+
+    def updated_at
+        nil
+    end
 end
