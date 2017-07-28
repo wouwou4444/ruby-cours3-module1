b79997c5a04893d534a51ec85c7986e02b258d33
wouwou4444 <wouwou4444@outlook.com>
2017-07-28 17:24:49 +0200

    will_paginate

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -84,4 +84,27 @@ class Racer
     def updated_at
         nil
     end
+
+    def self.paginate(params)
+        page=(params[:page] || 1).to_i
+
+        limit=(params[:per_page] || 30).to_i
+
+        skip=(page-1)*limit
+
+        racers=[]
+
+        docs = Racer.all.skip(skip).limit(limit)
+
+        docs.each do |doc| 
+            racers << Racer.new(doc)
+        end
+
+        total= Racer.all.count
+
+        WillPaginate::Collection.create(page, limit, total) do |pager|
+
+            pager.replace(racers)
+        end
+    end       
 end
